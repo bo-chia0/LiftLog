a379607b1ae88d76c3d959b3fda60434633a710b
bo-chia0 <108302067@g.nccu.edu.tw>
2023-12-22 18:35:51 +0800

    feat: User 註冊與登入功能 (controller)

---
+++ controllers/account_controllers.py
@@ -0,0 +1,22 @@
+"""
+這個檔案負責帳戶相關的功能，包括登入、註冊、忘記密碼等
+"""
+from models.user import User
+from models.session import session
+
+
+def register(email, password, username):
+    """
+    註冊新使用者
+    """
+    new_user = User(email=email, password=password, username=username)
+    session.add(new_user)
+    session.commit()
+
+
+def login(email, password):
+    """
+    登入使用者
+    """
+    user = session.query(User).filter_by(email=email, password=password).first()
+    return user
