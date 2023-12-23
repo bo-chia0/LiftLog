8fbcd62f990f179dc02da1d668e5411ed0bf28b7
bo-chia0 <108302067@g.nccu.edu.tw>
2023-12-23 18:44:07 +0800

    feat: 加入 CURRENT_WORKOUT_ID 全域參數

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 """
 定義 views 共用的參數
 """
-WIN_WIDTH = 396
-WIN_HEIGHT = 714
+WIN_WIDTH = 400
+WIN_HEIGHT = 800
 
 """
 目前使用的 User
 """
-USER_ID = None
+CURRENT_USER_ID = 1
+
+"""
+目前的 Workout
+"""
+CURRENT_WORKOUT_ID = 1
