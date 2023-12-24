fe6c8a432292be74d45416676a3581a2a4174d21
bo-chia0 <108302067@g.nccu.edu.tw>
2023-12-24 13:27:47 +0800

    feat: main.py 為 flet app 程式進入點

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+This is the entry point for your Flet application.
+Here, you would set up your Flet app and tie together the views and services.
+"""
+
+import flet as ft
+from views.login_view import login_page
+from views.home_view import home_page
+
+def main(page: ft.Page):
+    """
+    redirect 至登入畫面
+    """
+    # login_page(page)
+    home_page(page)
+
+if __name__ == "__main__":
+    ft.app(target=main)
+    
