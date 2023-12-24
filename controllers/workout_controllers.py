482307a8bd4d9f10a7f6b56ec835deef706f4992
bo-chia0 <108302067@g.nccu.edu.tw>
2023-12-24 14:52:55 +0800

    feat: 新增 workout controllers: 1. 取得最近 n 筆 workout_id 2. 取得指定 workout_id 的 user_name 和 start_time

--- controllers/workout_controllers.py
+++ controllers/workout_controllers.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from models.session import session
 from models.workout import Workout
+from models.user import User
     
 def add_workout(user_id: int) -> int:
     new_workout = Workout(
@@ -15,3 +16,17 @@ def end_current_workout(workout_id: int) -> None:
     workout = session.query(Workout).filter(Workout.id == workout_id).first()
     workout.end_time = datetime.now()
     session.commit()
+
+def get_last_n_workout_ids(n: int) -> list:
+    return [
+        workout.id for workout in session
+        .query(Workout)
+        .order_by(Workout.id.desc())
+        .limit(n)
+    ]
+
+def get_workout_info(workout_id: int) -> tuple():
+    workout = session.query(Workout).filter(Workout.id == workout_id).first()
+    user_name = session.query(User).filter(User.id == workout.user_id).first().username
+    date_time = workout.start_time.strftime("%Y-%m-%d %H:%M:%S")
+    return user_name, date_time
