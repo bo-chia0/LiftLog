7de016582151559df041feeb6a4fad30990fb6e4
bo-chia0 <108302067@g.nccu.edu.tw>
2023-12-29 09:20:12 +0800

    refactor: 重新排列主頁 layout

--- views/home_view.py
+++ views/home_view.py
@@ -1,23 +1,23 @@
 import flet as ft
 from config import GlobalConfig
-from views.components import header_logo, create_bottom_app_bar, dropdown_exercise, dropdown_muscle_group
+from views.components import create_bottom_app_bar, header_logo, dropdown_exercise, dropdown_muscle_group
 from controllers.set_controllers import get_largest_weight, get_largest_weight_for_exercise, get_exercise_max_weight_each_workout, get_muscle_groups_portion
 from controllers.workout_controllers import get_recent_n_months_workout_count, get_recent_n_months_workout_time
 from controllers.muscle_group_controllers import get_muscle_groups
 
 def home_page(page: ft.Page):
     """
-    Main Menu layout for the application.
+    主頁的頁面
     """
     # 視窗 properties
     page.title = "主頁"
     page.window_width = GlobalConfig.WIN_WIDTH
     page.window_height = GlobalConfig.WIN_HEIGHT
     
-    """ 2nd row: personal records """
-    # ------------------ 個人紀錄 ------------------ #
+    """ 2nd row: 個人與全體紀錄 (PR) """
+    # 個人紀錄 
     text_personal_record = ft.Text("個人紀錄", size=30)
-    text_personal_record_weight = ft.Text(size=50)
+    text_personal_record_weight = ft.Text("0", size=50)
     personal_record = ft.Column(
         controls=[
             text_personal_record,
@@ -25,9 +25,9 @@ def home_page(page: ft.Page):
         ],
         spacing=0
     )
-    # ------------------ 排名紀錄 ------------------ #
-    text_rank_record = ft.Text("排名紀錄", size=30)
-    text_rank_record_weight = ft.Text(size=50)
+    #  全體紀錄 
+    text_rank_record = ft.Text("全體紀錄", size=30)
+    text_rank_record_weight = ft.Text("0", size=50)
     text_rank_record_holder = ft.Text()
     rank_record = ft.Column(
         controls=[
@@ -41,7 +41,55 @@ def home_page(page: ft.Page):
         ],
         spacing=0
     )
+    record_container = ft.Container(
+        content=ft.Row(
+            controls=[
+                personal_record,
+                rank_record
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_AROUND
+        ), 
+        height=GlobalConfig.WIN_HEIGHT*0.15
+    )
+    
+    """ 3rd row: 重量趨勢折線圖 """
+    line_chart_container = ft.Container(
+        content = ft.LineChart(
+            horizontal_grid_lines=ft.ChartGridLines(
+                interval=5, width=0.5,
+                color=ft.colors.with_opacity(0.2, ft.colors.ON_SURFACE)
+            ),
+            vertical_grid_lines=ft.ChartGridLines(
+                interval=1, width=0.5,
+                color=ft.colors.with_opacity(0.2, ft.colors.ON_SURFACE)
+            ),
+            left_axis=ft.ChartAxis(
+                labels_size=0,
+                title=ft.Text("重量 (kg)"),
+                title_size=30
+            ),
+            bottom_axis=ft.ChartAxis(
+                labels_size=0,
+                title=ft.Text("訓練 (次)"),
+                title_size=30
+            ),
+            border=ft.border.all(1, ft.colors.GREY_400)
+        ),
+        height=GlobalConfig.WIN_HEIGHT*0.25
+    )
 
+    """ 1st row: 選擇訓練動作下拉選單 """
+    dropdown_container = ft.Container(
+        content=ft.Row(
+            controls=[
+                dropdown_muscle_group,
+                dropdown_exercise
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_AROUND
+        ),
+        height=GlobalConfig.WIN_HEIGHT*0.1
+    )
+    # Controls: 根據選擇的運動種類更新紀錄
     def update_records(e: ft.ControlEvent):
         # 更新個人紀錄
         text_personal_record_weight.value = (
@@ -67,60 +115,12 @@ def home_page(page: ft.Page):
                 curved=True
             )
         ]
-        line_chart_container.content = ft.LineChart(
-            data_series=line_chart_data,
-            horizontal_grid_lines=ft.ChartGridLines(
-                interval=5, width=0.5,
-                color=ft.colors.with_opacity(0.2, ft.colors.ON_SURFACE)
-            ),
-            vertical_grid_lines=ft.ChartGridLines(
-                interval=1, width=0.5,
-                color=ft.colors.with_opacity(0.2, ft.colors.ON_SURFACE)
-            ),
-            left_axis=ft.ChartAxis(
-                labels_size=0,
-                title=ft.Text("重量 (kg)"),
-                title_size=30
-            ),
-            bottom_axis=ft.ChartAxis(
-                labels_size=0,
-                title=ft.Text("訓練 (次)"),
-                title_size=30
-            ),
-            border=ft.border.all(1, ft.colors.GREY_400)
-        )
+        line_chart_container.content.data_series=line_chart_data
         e.page.update()
 
-    record_container = ft.Container(
-        content=ft.Row(
-            controls=[
-                personal_record,
-                rank_record
-            ],
-            alignment=ft.MainAxisAlignment.SPACE_AROUND
-        ), 
-        height=GlobalConfig.WIN_HEIGHT*0.15
-    )
-    
-    """ 3rd row: Line charts """
-    line_chart_container = ft.Container(
-        height=GlobalConfig.WIN_HEIGHT*0.25
-    )
-
-    """ 1st row: dropdown menus """
     dropdown_exercise.on_change = update_records
-    dropdown_container = ft.Container(
-        content=ft.Row(
-            controls=[
-                dropdown_muscle_group,
-                dropdown_exercise
-            ],
-            alignment=ft.MainAxisAlignment.SPACE_AROUND
-        ),
-        height=GlobalConfig.WIN_HEIGHT*0.1
-    )
 
-    """ 4th row: Bar charts """
+    """ 4th row: 每月訓練次數統計圖 """
     workout_count_chart = ft.BarChart(
         bar_groups=[
             ft.BarChartGroup(
@@ -162,9 +162,9 @@ def home_page(page: ft.Page):
         height=GlobalConfig.WIN_HEIGHT*0.3
     )
 
-    """ 5th row: Pie charts """
+    """ 5th row: 訓練部位統計圓餅圖 """
     muscle_groups = get_muscle_groups()
-    color = [
+    colors = [
         ft.colors.AMBER, ft.colors.BLUE, ft.colors.GREEN, ft.colors.LIME, ft.colors.ORANGE, ft.colors.PINK
     ]
     muscle_group_proportion_pie_chart = ft.PieChart(
@@ -172,7 +172,7 @@ def home_page(page: ft.Page):
             ft.PieChartSection(
                 100*get_muscle_groups_portion(i, GlobalConfig.CURRENT_USER_ID),
                 title=f"{muscle_groups[i]}\n{100*get_muscle_groups_portion(i, GlobalConfig.CURRENT_USER_ID):.2f}%",
-                color=color[i]
+                color=colors[i]
             ) for i in range(6)
         ]
     )
@@ -181,7 +181,7 @@ def home_page(page: ft.Page):
         height=GlobalConfig.WIN_HEIGHT*0.25
     )
 
-    """ 6th row: Bar Chart """
+    """ 6th row: 每月累績訓練時間統計圖 """
     workout_time_chart = ft.BarChart(
         bar_groups=[
             ft.BarChartGroup(
@@ -223,18 +223,22 @@ def home_page(page: ft.Page):
         height=GlobalConfig.WIN_HEIGHT*0.25
     )
 
-    """ Organize the layout using Columns """
+    """ 整合所有統計元素 """
     statistics = ft.Container(
         ft.Column(
                 controls=[
                     dropdown_container,
                     record_container,
+                    ft.Text("重量趨勢", size=20),
                     line_chart_container,
+                    ft.Text("訓練頻率", size=20),
                     bar_chart_container,
+                    ft.Text("訓練部位分佈", size=20),
                     pie_chart_container,
+                    ft.Text("每月累積訓練時間", size=20),
                     workout_time_container
                 ],
-                scroll=ft.ScrollMode.ALWAYS
+                scroll=ft.ScrollMode.ALWAYS, spacing=20
         ),
         height=GlobalConfig.WIN_HEIGHT*0.75
     )       
@@ -242,7 +246,7 @@ def home_page(page: ft.Page):
     """ navigation bar """
     page.bottom_appbar = create_bottom_app_bar()
 
-    # Organize the layout using Rows and Columns
+    """ 整合所有頁面元素 """
     page.add(
         ft.Column(
             controls=[
